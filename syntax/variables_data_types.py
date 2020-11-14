10a58d3fcbfd4bf1bf893e428d2f9f46b23a66d5
Christian Cain <christian22cain@yahoo.com>
2020-11-14 12:49:49 -0600

    studied variables & data types

---
+++ syntax/variables_data_types.py
@@ -0,0 +1,38 @@
+# -- VARIABLES --
+# Naming should only include letters, numbers, and underscores
+# Must start with a letter
+# Stick to lowercase!
+# Refrain from using reserved words
+
+
+message = "hello world!"
+# print(message) // => "hello world!"
+
+message = "hello world 2"
+# print(message) // => "hello world 2"
+
+
+name = "christian cain"
+# print(name.title()) //=> "Christian Cain"
+# print(name.upper()) // => "CHRISTIAN CAIN"
+# print(name.lower()) // => "christian cain" 
+
+
+# print(f"Hello {name.title()}!") // => "Hello Christian Cain!"
+
+
+# print("\tThis indents the text!") // => Indents text
+# print("languages:\nPython") // => Writes on new line
+# print("languages:\n\tPython\n\tJavaScript\n\tRuby on Rails")
+
+
+# -- NUMBERS --
+
+
+x_int = 1
+x_float = 1.0
+x_long_num = 1_000_000
+# print(x_long_num + x_float) // => 1000001.0
+
+x, y, z = 1, 2, 3
+# print(x, y, z) // => 1 2 3
