ca3f462ddec13ac5dde5be64b657005c51ce8414
Christian Cain <christian22cain@yahoo.com>
2020-11-17 11:36:44 -0600

    learned about ui & while loops

---
+++ syntax/user_input_and_while_loops.py
@@ -0,0 +1,33 @@
+# -- USER INPUT & WHILE LOOPS --
+# Creating CLI applications
+# Inputs return as Strings!
+from random import randrange
+
+# message = input('What is your name: ')
+# print(message)
+
+# age = input('How old are you: ')
+# age = int(age)
+# if(age > 18):
+# 	print('You are old enough to vote!')
+# else:
+# 	print('You are not old enough to vote!')
+
+# current_num = 1
+# while current_num < 5:
+# 	print(current_num)
+# 	current_num +=1
+
+# -- Num Guess --
+# should_continue = True
+# num = randrange(10)
+# print('Guess a number between 1 & 10')
+# while should_continue:
+# 	guess = input('\nWhat is your guess? ')
+# 	guess = int(guess)
+# 	if(guess == num):
+# 		print(f'Correct! {guess} is the right answer!')
+# 		should_continue = False
+# 	else:
+# 		print('Sorry! Guess again!')
+
